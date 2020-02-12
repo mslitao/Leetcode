d99014df02a6429ac3fe3e72d1d7ffc57466da51
Tao Li <mslitao@outlook.com>
2020-02-11 23:06:28 -0800

    Update 15.3sum.cs

--- 15.3sum.cs
+++ 15.3sum.cs
@@ -62,4 +62,44 @@ public class Solution15
         }
         return data;
     }
+
+    public IList<IList<int>> ThreeSum2(int[] nums)
+    {
+        IList<IList<int>> results = new List<IList<int>>();
+        if(nums == null || nums.Length < 3) return results;
+        
+        // Step 1: sort the data.
+        Array.Sort(nums);
+
+        for(int i =0; i < nums.Length - 2; ++i)
+        {
+            //Early stop
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int target = -1 * nums[i];
+            
+            int j = i + 1;
+            int k = nums.Length -1;
+            while(j < k)
+            {
+                if(nums[j] + nums[k] == target)
+                {
+                    results.Add(new List<int>(){nums[i], nums[j], nums[k]});
+                    while(j < k && nums[j] == nums[++j]);
+                    while(j < k && nums[k] == nums[--k]);
+                }
+                else if(nums[j] + nums[k] < target)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+
+        return results;
+    }
 }
