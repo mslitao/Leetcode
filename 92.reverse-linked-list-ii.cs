856956351d5b47498a6e2fe8a4c15419c4a24232
Tao Li <mslitao@outlook.com>
2020-02-16 10:16:07 -0800

    Create 92.reverse-linked-list-ii.cs

---
+++ 92.reverse-linked-list-ii.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Solution92
+{
+
+    //Definition for singly-linked list.
+    public class ListNode 
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) 
+        {
+            val = x; 
+        }
+    }
+
+    public ListNode ReverseBetween(ListNode head, int m, int n)
+    {
+        if (head == null) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy; // make a pointer pre as a marker for the node before reversing
+        for (int i = 0; i < m - 1; i++) pre = pre.next;
+
+        ListNode start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
+        ListNode then = start.next; // a pointer to a node that will be reversed
+
+        // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+
+        for (int i = 0; i < n - m; i++)
+        {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+
+        // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+        // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+
+        return dummy.next;
+    }
+}
