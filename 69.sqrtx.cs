59296ea261ab9d37eec1359919dc1777c4910216
Tao Li <mslitao@outlook.com>
2020-02-15 21:31:27 -0800

    Create 69.sqrtx.cs

---
+++ 69.sqrtx.cs
@@ -0,0 +1,16 @@
+
+
+public class Solution69 {
+    public int MySqrt(int x) {
+        int i = 1, j = x;
+        while(i < j){
+            int mid = i + (j - i)/2;
+            if(x / mid < mid)
+                j = mid;
+            else
+                i = mid + 1;
+        }
+            
+        return x / i < i ? i - 1 : i;
+    }
+}
