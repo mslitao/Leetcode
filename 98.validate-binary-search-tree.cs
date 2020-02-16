e5ffdd5c8b5cbd0a2d85f2ac9c180e300efbb5ce
Tao Li <mslitao@outlook.com>
2020-02-16 13:57:42 -0800

    Update 98.validate-binary-search-tree.cs

--- 98.validate-binary-search-tree.cs
+++ 98.validate-binary-search-tree.cs
@@ -13,5 +13,28 @@ public class Solution98
         public TreeNode(int x) { val = x; }
     }
 
-    
+    public bool IsValidBST(TreeNode root) 
+    {
+        if (root == null)
+        {
+            return true;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode prev = null;
+        while (root != null || stack.Count != 0)
+        {
+            while (root != null)
+            {
+                stack.Push(root);
+                root = root.left;
+            }
+
+            root = stack.Pop();
+            if (prev != null && root.val <= prev.val) return false;
+            prev = root;
+            root = root.right;
+        }
+
+        return true;
+    }
 }
