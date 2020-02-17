c8b685945009384dab316e320ebbf85c5f9a8050
Tao Li <mslitao@outlook.com>
2020-02-16 21:17:36 -0800

    Update 113.path-sum-ii.cs

--- 113.path-sum-ii.cs
+++ 113.path-sum-ii.cs
@@ -14,34 +14,34 @@ public class Solution113
     }
 
     public IList<IList<int>> PathSum(TreeNode root, int sum) {
-            IList<IList<int>> results = new List<IList<int>>();
-            IList<int> values = new List<int>();
-            DFSPathSum(root, sum, 0, values, results);
+        IList<IList<int>> results = new List<IList<int>>();
+        IList<int> values = new List<int>();
+        DFSPathSum(root, sum, 0, values, results);
 
-            return results;
-        }
+        return results;
+    }
 
-        public void DFSPathSum(TreeNode node, int target,  int sum, IList<int> values, IList<IList<int>> results)
+    public void DFSPathSum(TreeNode node, int target,  int sum, IList<int> values, IList<IList<int>> results)
+    {
+        if(node == null)
         {
-            if(node == null)
-            {
-                return;
-            }
-
-            if(node.left == null && node.right == null)
-            {
-                if(sum + node.val == target)
-                {
-                    var res = values.Select(n=>n).ToList();
-                    res.Add(node.val);
-                    results.Add(res);
-                }
-                return;
-            }
-
-            var newValues = new List<int>(values);
-            newValues.Add(node.val);
-            DFSPathSum(node.left, target, sum + node.val, newValues, results);
-            DFSPathSum(node.right, target, sum + node.val, newValues, results);
+            return;
         }
+
+        if(node.left == null && node.right == null)
+        {
+            if(sum + node.val == target)
+            {
+                var res = values.Select(n=>n).ToList();
+                res.Add(node.val);
+                results.Add(res);
+            }
+            return;
+        }
+
+        var newValues = new List<int>(values);
+        newValues.Add(node.val);
+        DFSPathSum(node.left, target, sum + node.val, newValues, results);
+        DFSPathSum(node.right, target, sum + node.val, newValues, results);
+    }
 }
