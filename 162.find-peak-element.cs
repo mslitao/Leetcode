bae2cae2669373f26895de3f3529ea603cfd9cc2
Tao Li <mslitao@outlook.com>
2020-03-26 22:00:27 -0700

    Update 162.find-peak-element.cs
    Binary Search - Examples

--- 162.find-peak-element.cs
+++ 162.find-peak-element.cs
@@ -12,4 +12,43 @@ public class Solution162
         }
         return nums.Length - 1;
     }
+
+    public int FindPeakElement_BinarySearch(int[] nums) {
+        return BPFindPeakElement(nums, 0, nums.Length -1 );
+    }
+
+    public int BPFindPeakElement(int[] nums, int low, int high)
+    {
+        if(low == high) return low;
+        if(low + 1 == high) 
+            return nums[low] > nums[high] ? low: high;
+
+        int middle = low + (high - low) / 2;
+        if(nums[middle] > nums[middle - 1] && nums[middle] > nums[middle -1])
+        {
+            return middle;
+        }
+        else if (nums[middle -1] > nums[middle])
+        {
+            return BPFindPeakElement(nums, low, middle -1);
+        }
+        else
+        {
+            return BPFindPeakElement(nums, middle + 1, high);
+        }
+    }
+
+    public int FindPeakElement_BinarySearch2(int[] nums) 
+    {
+        int left = 0; 
+        int right = nums.Length -1;
+        while(left < right)
+        {
+            int middle = left + (right - left) / 2;
+            if(nums[middle] < nums[middle + 1]) left = middle + 1;
+            else
+                right = middle; 
+        }
+        return BPFindPeakElement(nums, 0, nums.Length -1 );
+    }
 }
