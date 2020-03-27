ef71b7b4a0fd31bf3de3d9c5b018cd90d5bb4f25
Tao Li <mslitao@outlook.com>
2020-03-26 19:09:27 -0700

    Update 515.find-largest-value-in-each-tree-row.cs

--- 515.find-largest-value-in-each-tree-row.cs
+++ 515.find-largest-value-in-each-tree-row.cs
@@ -37,31 +37,4 @@ public class Solution515
 
         return results;
     }
-
-    public int MaxDepth(Node root) {
-        if(root == null) return 0;
-        int depth = 0;
-        
-        Queue<Node> queue = new Queue<Node>();
-        queue.Enqueue(root);
-
-        while(queue.Any())
-        {
-            depth ++;
-            int cnt = queue.Count;
-            for(int i =0; i< cnt; ++i)
-            {
-                var node = queue.Dequeue();
-                if(node.children == null || node.children.Count ==0)
-                    continue;
-
-                foreach(var child in node.children)
-                {
-                    queue.Enqueue(child);
-                }
-            }
-        }
-
-        return depth;
-    }
 }
