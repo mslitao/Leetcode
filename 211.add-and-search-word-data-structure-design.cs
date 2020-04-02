6bf570122f20759342224762df5941321ff7dafa
Tao Li <mslitao@outlook.com>
2020-04-01 23:31:30 -0700

    Create 211.add-and-search-word-data-structure-design.cs

---
+++ 211.add-and-search-word-data-structure-design.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Solution211
+{
+
+}
+
+public class WordDictionary
+{
+    public class TrieNode
+    {
+        public TrieNode[] Next;
+        public bool IsWord;
+
+        public TrieNode()
+        {
+            this.Next = new TrieNode[26];
+            this.IsWord = false;
+        }
+    }
+
+    TrieNode trie;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        this.trie = new TrieNode();
+    }
+    
+    /** Adds a word into the data structure. */
+    public void AddWord(string word) {
+        TrieNode current = this.trie;
+        int i = 0;
+        for(; i < word.Length && current.Next[word[i] - 'a'] != null; ++i)
+        {
+            current = current.Next[word[i] - 'a'];
+        }
+
+        for(; i < word.Length; ++i)
+        {
+            current.Next[word[i] - 'a'] = new TrieNode();
+            current = current.Next[word[i] - 'a'];
+        }
+        current.IsWord = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public bool Search(string word) {
+        return SearchBFS(word);
+    }
+
+    public bool SearchBFS(string word) {
+        Queue<TrieNode> queue = new Queue<TrieNode>();
+        queue.Enqueue(this.trie);
+
+        for(int index = 0; index < word.Length && queue.Any(); ++ index)
+        {
+            int cnt = queue.Count;
+            for(int i =0; i < cnt; ++i)
+            {
+                TrieNode node = queue.Dequeue();
+                if(word[index] == '.')
+                {
+                    foreach(var next in node.Next)
+                    {
+                        if(next != null)
+                            queue.Enqueue(next);
+                    }
+                }
+                else
+                {
+                    if(node.Next[word[index] - 'a'] != null)
+                    {
+                        queue.Enqueue(node.Next[word[index] - 'a']);
+                    }
+                }
+            }
+        }
+        
+        while(queue.Any())
+            if(queue.Dequeue().IsWord) return true;
+        
+        return false;
+    }
+}
