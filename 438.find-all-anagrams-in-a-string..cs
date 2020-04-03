a166b4701418c73b787de3d492c8dd2eefcbb74d
Tao Li <mslitao@outlook.com>
2020-04-02 21:55:51 -0700

    Update 438.find-all-anagrams-in-a-string..cs

--- 438.find-all-anagrams-in-a-string..cs
+++ 438.find-all-anagrams-in-a-string..cs
@@ -2,10 +2,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Solution438 {
+public class Solution438 
+{
+    public IList<int> FindAnagrams_2ndTry(string s, string p) {
+        List<int> results = new List<int>();
+        Dictionary<char, int> pMap = new Dictionary<char, int>();
+        foreach(var c in p)
+        {
+            if(!pMap.ContainsKey(c))
+            {
+                pMap.Add(c, 1);
+            }
+            else
+            {
+                pMap[c]++;
+            }
+        }
+
+        Dictionary<char, int> sMap = new Dictionary<char, int>();
+        foreach(var item in pMap)
+        {
+            sMap.Add(item.Key, 0);
+        }
+        int left = 0;
+        int right = 0;
+        int size = p.Length;
+        while(right < s.Length)
+        {
+            if(sMap.ContainsKey(s[right]) && ++sMap[s[right]] <= pMap[s[right]])
+            {
+                size --;
+            }
+            right ++;
+            if(right - left > p.Length)
+            {
+                if(sMap.ContainsKey(s[left]) && --sMap[s[left]] < pMap[s[left]])
+                {
+                    size ++;
+                }
+                left ++;
+            }
+
+            if(size == 0)
+            {
+                results.Add(left);
+            }
+        }
+
+        return  results;
+    }
 
     // using sliding window, every time, just move once.
-    public IList<int> FindAnagrams(string s, string p) {
+    public IList<int> FindAnagrams(string s, string p) 
+    {
         IList<int> results = new List<int>();
         if(string.IsNullOrEmpty(s) || string.IsNullOrEmpty(p) || s.Length < p.Length) return results;
 
@@ -35,4 +84,5 @@ public class Solution438 {
 
         return results;
     }
+
 }
