403409fbff05c72b8af2b2ff11fc4eaa24a4e3bc
Tao Li <mslitao@outlook.com>
2020-04-02 23:30:16 -0700

    Update 215.kth-largest-element-in-an-array.cs

--- 215.kth-largest-element-in-an-array.cs
+++ 215.kth-largest-element-in-an-array.cs
@@ -5,12 +5,50 @@ using System.Text;
 
 public class Solution215
 {
+    public void swap(int[] nums, int i , int j)
+    {
+        var tmp = nums[i];
+        nums[i] =nums[j];
+        nums[j]= tmp;
+    }
+    
+    public int FindKthLargest_2ndTry(int[] nums, int k) {
+        int left =0;
+        int right = nums.Length - 1;
+        while(true)
+        {
+            int pos = Partition(nums, left, right;
+            if(pos == k - 1) return nums[pos];
+            else if (pos > k -1)  right = pos -1;
+            else left = pos + 1;
+        }
+    }
+
+    public int Partition(int[] nums, int left, int right)
+    {
+        int pivot = nums[left];
+        int l = left + 1;
+        int r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums, l++, r--);
+            }
+            if (nums[l] >= pivot) ++l;
+            if (nums[r] <= pivot) --r;
+        }
+        swap(nums, left, r);
+        return r;
+    }
+
+
     public int FindKthLargest(int[] nums, int k) {
         QuickSort(nums, 0, nums.Length -1);
 
         //Console.WriteLine(string.Join('-', nums));
         return nums[nums.Length - k];
     }
+
+   
 
     public void QuickSort(int[] nums, int start, int end)
     {
@@ -59,10 +97,5 @@ public class Solution215
         }
     }
     
-    public void swap(int[] nums, int i , int j)
-    {
-        var tmp = nums[i];
-        nums[i] =nums[j];
-        nums[j]= tmp;
-    }
+    
 }
