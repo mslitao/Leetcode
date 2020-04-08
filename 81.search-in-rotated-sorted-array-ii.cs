52a009769ab3c9374f90c8c3cf1dfa871db758de
Tao Li <mslitao@outlook.com>
2020-04-07 20:35:17 -0700

    Update 81.search-in-rotated-sorted-array-ii.cs

--- 81.search-in-rotated-sorted-array-ii.cs
+++ 81.search-in-rotated-sorted-array-ii.cs
@@ -8,7 +8,7 @@ public class Solution81
     public bool Search(int[] nums, int target) {
         if(nums == null || nums.Length == 0 ) return false;
 
-        return BinarySearch(nums, target, 0, nums.Length -1)
+        return BinarySearch(nums, target, 0, nums.Length -1);
     }
 
     public bool BinarySearch(int[] nums, int target, int left, int right)
