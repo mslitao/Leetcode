c21ee26aefb93fcdb9041691cf9280fd9f184a21
Tao Li <mslitao@outlook.com>
2020-04-19 20:50:35 -0700

    InvertTree

--- mock.0419.cs
+++ mock.0419.cs
@@ -29,4 +29,21 @@ class SolutionMock0419
 
         return result;
     }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode InvertTree(TreeNode root) {
+        if(root == null) return null;
+        var tmp = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(tmp);
+
+        return root;
+    }
 }
