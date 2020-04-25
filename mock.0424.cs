c6f555f07be70a0d0d379b7d2b0fb6313c3ea36c
Tao Li <mslitao@outlook.com>
2020-04-24 22:49:38 -0700

    Update the code

--- mock.0424.cs
+++ mock.0424.cs
@@ -106,4 +106,37 @@ class SolutionMock0424
         }
         result.Add(cur);
     }
+
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        else if(p == null || q == null) return false;
+        else return  p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right); 
+    }
+
+    public double MyPow(double x, int n) {
+        if(n == 0) return 1;
+
+        double half = MyPow(x, n/2);
+        if(n % 2 == 0) return half * half;
+        if(n > 0) return half*half*x;
+        else return half*half/x;
+    }
+
+    public int LargestRectangleArea(int[] heights) {
+        if(heights == null || heights.Length == 0) return 0;
+        int maxArea = 0;
+
+        for(int i = 0; i < heights.Length; ++i)
+        {
+            int minHeight = heights[i];
+            maxArea = Math.Max(maxArea, minHeight);
+            for(int j = i - 1; j >=0; --j)
+            {
+                minHeight = Math.Min(minHeight, heights[j]);
+                maxArea = Math.Max(maxArea, minHeight * (i - j));
+            }
+        }
+
+        return maxArea;
+    }
 }
