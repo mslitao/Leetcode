b779b209145e288a5578746722e2b668b4120987
Tao Li <mslitao@outlook.com>
2020-04-26 14:52:55 -0700

    Update mock.0426.cs

--- mock.0426.cs
+++ mock.0426.cs
@@ -82,6 +82,7 @@ class SolutionMock0426
         
         return checkL || checkR;
     }
+
     public Tuple<TreeNode, int> RecursiveUpdate(TreeNode root, int limit) 
     {
         if(root == null) return Tuple.Create((TreeNode)null, 0);
@@ -109,5 +110,112 @@ class SolutionMock0426
         var maxPath = Math.Max(pathL, pathR) + root.val;
 
         return maxPath > limit? Tuple.Create(root, maxPath) : Tuple.Create((TreeNode)null, 0);
+    }
+
+    public int RemoveDuplicates(int[] nums) {
+        int lastNum = -1;
+        int len = 0;
+        for(int i = 0; i < nums.Length; ++ i)
+        {
+            if(i == 0)
+            {
+                lastNum = nums[i];
+                len ++;
+            }
+            else
+            {
+                if(nums[i] != lastNum)
+                {
+                    nums[len ] = nums[i];
+                    lastNum = nums[i];
+                    len ++;
+                }
+            }
+        }
+
+        return len;
+    }
+
+    public IList<string> RestoreIpAddresses(string s) {
+        List<string> current  = new List<string>();
+        List<string> results = new List<string>();
+
+        BacktrackingIPAddress(s, current, results);
+        return results;
+    }
+
+    public void BacktrackingIPAddress(string s, List<string> current, List<string> result)
+    {
+        if(string.IsNullOrEmpty(s) &&  current.Count == 4)
+        {
+            result.Add(string.Join('.', current));
+        }
+        else if (string.IsNullOrEmpty(s) || current.Count == 4)
+        {
+            return;
+        }
+
+        for(int i = 1; i <= 3; ++i)
+        {
+            if(i > s.Length) 
+                break;
+            var str = s.Substring(0, i);
+            if(i > 1 && s.StartsWith("0")) continue;
+            var part = int.Parse(str);
+            if(part >=0 && part<=255)
+            {
+                var left = i == s.Length ? "" : s.Substring(i);
+                int pos = current.Count;
+                current.Add(str);
+                BacktrackingIPAddress(left, current, result);
+                current.RemoveAt(pos);
+            }
+        }
+    }
+
+    public int Candy(int[] ratings) {
+        Stack<int> stack = new Stack<int>();
+        int[] candies = new int[ratings.Length];
+        int candy = 1;
+        for(int i = 0; i < ratings.Length; ++ i)
+        {
+            if(i == 0 )
+            {
+                stack.Push(i);
+            }
+            else if (ratings[i] < ratings[i - 1])
+            {
+                stack.Push(i);
+            }
+            else
+            {
+                candy = 1;
+                while(stack.Any())
+                {
+                    candies[stack.Pop()] =candy ++;
+                }
+                
+                if(ratings[i] == ratings[i - 1])
+                    candies[i] = 1;
+                else
+                    candies[i] = candies[i -1 ] + 1;
+            }
+        }
+
+        candy =1;
+        while(stack.Any())
+        {
+            candies[stack.Pop()] =candy ++;
+        }
+
+        for(int i = ratings.Length -2; i >=0; i --)
+        {
+            if(ratings[i] > ratings[i + 1] && candies[i] < candies[i + 1])
+            {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+
+        return candies.Sum();
     }
 }
