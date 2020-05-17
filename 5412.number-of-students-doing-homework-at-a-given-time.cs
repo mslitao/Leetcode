779eb5df2b6d5546374a7c9aab9bcb28596e9036
Tao Li <mslitao@outlook.com>
2020-05-16 22:23:15 -0700

    Create 5412.number-of-students-doing-homework-at-a-given-time.cs

---
+++ 5412.number-of-students-doing-homework-at-a-given-time.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution5412
+{
+    public int BusyStudent(int[] startTime, int[] endTime, int queryTime) {
+        
+    }
+}
