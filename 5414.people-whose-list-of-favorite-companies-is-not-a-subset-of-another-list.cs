042331efed2df3a8f933e4e0d811ba372648a02e
Tao Li <mslitao@outlook.com>
2020-05-16 22:23:24 -0700

    Create 5414.people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.cs

---
+++ 5414.people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution5414
+{
+    public IList<int> PeopleIndexes(IList<IList<string>> favoriteCompanies) {
+        Dictionary<string, HashSet<int>> map = new Dictionary<string, HashSet<int>>();
+
+        
+        for(int i = 0; i < favoriteCompanies.Count; ++i)
+        {
+            foreach(var fc in favoriteCompanies[i])
+            {
+                if(!map.ContainsKey(fc))
+                {
+                    map.Add(fc, new HashSet<int>());
+                }
+
+                map[fc].Add(i);
+            }
+        }
+
+        HashSet<int> results = new HashSet<int>();
+        for(int i = 0; i < favoriteCompanies.Count; ++i)
+        {
+            var bc = map[favoriteCompanies[i][0]];
+
+            foreach(var idx in bc)
+            {
+                if(idx == i) continue;
+                bool match = true;
+                foreach(var company in favoriteCompanies[i])
+                {
+                    if(!map[company].Contains(idx))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if(match)
+                {
+                    results.Add(i);
+                    break;
+                }
+            }
+        }
+
+        var left = new List<int>();
+        
+        for(int i = 0; i < favoriteCompanies.Count; ++i)
+        {
+            if(!results.Contains(i))
+            {
+                left.Add(i);
+            }
+        }
+        return results;
+
+
+    }
+}
