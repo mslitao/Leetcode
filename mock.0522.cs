c0fc4dccd3d3eb14e8e21b8b89b4f20441ffefcc
Tao Li <mslitao@outlook.com>
2020-05-22 22:03:15 -0700

    Create mock.0522.cs

---
+++ mock.0522.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SolutionMock0522 {
+    public int BulbSwitch(int n) {
+        bool[] bulbs = new bool[n];
+        Array.Fill(bulbs, true);
+
+        for(int start = 1; start < n; ++ start)
+        {
+            for(int j = start; j < n ; j = j + start + 1)
+            {
+                bulbs[j] = !bulbs[j];
+            }
+        }
+
+        int result = 0;
+        for(int i = 0; i < n; ++ i)
+        {
+            if(bulbs[i]) result ++;
+        }
+
+        return result;
+    }
+}
